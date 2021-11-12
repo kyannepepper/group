a0c558dd855a578c66db561e9bf3a0e06cadb7b7
Slade <sladehirsc@gmail.com>
2021-11-12 14:34:05 -0700

    Added tab navigator

--- App.js
+++ App.js
@@ -1,47 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Dimensions } from 'react-native';
-import { StyleSheet, Text, View, TextInput, Button, Image, ImageBackground, TouchableHighlight } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Dimensions, Image, ImageBackground, TouchableHighlight } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import groupsScreen from './screens/groupsScreen';
+import servicesScreen from './screens/servicesScreen';
+
+
+const Tab = createBottomTabNavigator();
+
 
 export default function App() {
-  const [text, onChangeText] = React.useState("Useless Text");
-  const [number, onChangeNumber] = React.useState(null);
+  const [text, onChangeText] = useState("Useless Text");
+  const [number, onChangeNumber] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+
   return (
-    <View style={styles.container}>
+    <>
+    {isLoggedIn ? 
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Group" component={groupsScreen} />
+          <Tab.Screen name="Services" component={servicesScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+      : 
+      <View style={styles.container}>
       <Image
-  source={ require("./assets/logo.png") }
-  style={{margin: 30, width: 100, height: 50, resizeMode: 'contain' }}></Image>
-    <View style={styles.haveaccount}>
-    <ImageBackground resizeMode="cover"
-  source={ require("./assets/people.png") }
-  style={{ width: Dimensions. get('window'). width, height: 500, flex: 1, justifyContent: 'flex-start', alignItems: 'center', paddingTop: 375,
-  justifyContent: 'center', }}>
-    <TouchableHighlight onPress={()=>{this.onButtonPress()}}
-        style={{
-          justifyContent: "center",
-          width: 300,
-          height: 50,
-          alignItems: "center",
-          backgroundColor: "#7839a4",
-          borderRadius: 5,
-        }}>
-         <Text style={{color: 'white'}}>Get Started</Text>
-      </TouchableHighlight>
-    <View style={{flex: 1, flexDirection: 'row', width: Dimensions. get('window'). width, alignItems: 'center', justifyContent: 'center',}}><Text style={styles.blue}>Already Have an account?</Text><TouchableHighlight onPress={()=>{this.onButtonPress()}}><Text style={styles.blue}>Login Here</Text></TouchableHighlight></View> 
-</ImageBackground>
-  </View>
-    </View>
+        source={ require("./assets/logo.png") }
+        style={{margin: 30, width: 100, height: 50, resizeMode: 'contain' }}>
+      </Image>
+          <View style={styles.haveaccount}>
+            <ImageBackground resizeMode="cover"
+              source={ require("./assets/people.png") }
+              style={{ width: Dimensions. get('window'). width, height: 500, flex: 1, justifyContent: 'flex-start', alignItems: 'center', paddingTop: 375,
+              justifyContent: 'center', }}
+            >
+                <TouchableHighlight onPress={() => setIsLoggedIn(true)}
+                  style={{
+                    justifyContent: "center",
+                    width: 300,
+                    height: 50,
+                    alignItems: "center",
+                    backgroundColor: "#7839a4",
+                    borderRadius: 5,
+                  }}
+                >
+                  <Text style={{color: 'white'}}>Get Started</Text>
+                </TouchableHighlight>
+                <View style={{flex: 1, flexDirection: 'row', width: Dimensions. get('window'). width, alignItems: 'center', justifyContent: 'center',}}><Text style={styles.blue}>Already Have an account?</Text><TouchableHighlight onPress={()=>{this.onButtonPress()}}><Text style={styles.blue}>Login Here</Text></TouchableHighlight></View> 
+            </ImageBackground>
+          </View>
+        </View>
+      }
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    height: Dimensions. get('window'). height,
+    height: Dimensions.get('window'). height,
     backgroundColor: '#1B1D21',
-   
     justifyContent: 'space-between',
-    
   },
   btcontainer: {
    flex: 1,
---
+++ screens/groupsScreen.js
@@ -0,0 +1,11 @@
+import React, {useEffect} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function groupsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Groups!</Text>
+    </View>
+  );
+}
