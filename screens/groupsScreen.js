908e08c53455118aef8b9c9d4f183d1a45ac48ab
Kyanne <7pepperklein@gmail.com>
2021-11-13 00:54:02 -0700

    A lot of styling

--- screens/groupsScreen.js
+++ screens/groupsScreen.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Dimensions } from 'react-native';
-import { StyleSheet, Text, View, TextInput, Button, Image, ImageBackground, TouchableHighlight } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TextInput, Button, Image, ImageBackground, TouchableHighlight } from 'react-native';
 import { withSafeAreaInsets } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 export default function groupsScreen() {
@@ -14,6 +14,7 @@ export default function groupsScreen() {
 
   return (
     <View style={styles.container}>
+    
     <View style={{backgroundColor: '#7839a4', width: Dimensions. get('window'). width, alignItems: 'center', height: 100, justifyContent: 'space-between', flex: 0, flexDirection: 'row', paddingTop:20,}}>
     <Image
   source={ require("../assets/logo.png") }
@@ -22,11 +23,11 @@ export default function groupsScreen() {
   source={ require("../assets/settings.png") }
   style={{marginLeft: 30, marginBottom: 0, width: 100, height: 50, resizeMode: 'contain' }}></Image>
     </View>
-
+    <ScrollView style={styles.scrollView}>
 
     <View style={styles.infobox}><Text style={{color: 'white', fontSize: 30, textAlign: 'center',}}>+ Create Group</Text></View>
     
-
+    </ScrollView>
 
     </View>
   );
@@ -40,47 +41,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     
   },
-  holder: {
-    flex: 1,
-    width: Dimensions. get('window'). width,
-    paddingLeft: 50,
-  },
-  formtitles: {
-    fontSize: 20,
-    color: 'white',
-  },
-  inputs: {
-    height: 35,
-    fontSize: 17,
-    borderBottomColor: '#979797',
-    borderBottomWidth: 2,
-    width: 250,
-    margin: 10,
-    color: 'white'
-  },
-  btcontainer: {
-   flex: 1,
-   padding: 30,
-  },
-  haveaccount: {
-
-  height: 500
-  },
-  hatext: {
-    height: 100,
-    width: Dimensions. get('window'). width,
-    backgroundColor: '#CDCDCD',
-  },
-  red: {
-    fontSize: 30,
-    color: '#d1d2d3',
-  },
-  blue: {
-    fontSize: 17,
-    color: '#979797',
-    textAlign: "center",
-    margin: 3,
-  },
+  scrollView: {
+    backgroundColor: '#1B1D21',
+    height:9000
+},
   infobox: {
     backgroundColor: '#323232',
     width: (Dimensions. get('window'). width) * .9,
